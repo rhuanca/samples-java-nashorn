2b87b733b883e2cfce1d28d6fc8811463a5f9727
Renan Huanca <renan.huanca@mojix.com>
2018-04-04 21:53:27 -0400

    added example

--- src/main/resources/hello.js
+++ src/main/resources/hello.js
@@ -1,4 +1,22 @@
-var a = 1
-var b = 1
-var c = a + b
-logger.info("result: " +c );
+//var a = 1
+//var b = 1
+//var c = a + b
+//logger.info("result: " +c );
+//
+
+var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var filtered = data.filter(function(i) {
+  return i % 2 == 0;
+});
+//print(filtered);
+
+logger.info(filtered);
+
+var sumOfFiltered = filtered.reduce(function(acc, next) {
+  return acc + next;
+}, 0);
+//print(sumOfFiltered); 
+
+logger.info(sumOfFiltered);
+
